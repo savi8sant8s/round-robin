f277a1a336ccdf886e7c042f55b4c751d5cd72e4
Savio Santos <saviosa08@gmail.com>
2021-05-23 09:07:40 -0300

    :wrench: Updated function comments

--- src/lib.rs
+++ src/lib.rs
@@ -2,20 +2,21 @@
 ///Create a vector of rounds and their respective matches in a tournament, 
 ///using the Round-robin algorithm.
 ///
-///Accepted:
-///-- number of even players;
-///-- round-trip;
+///<strong>Accepted</strong>:
+///- generic data types;
+///- number of even players;
+///- round-trip.
 /// 
 ///#### Examples with numbers:
 ///```
-///let rounds1 = round_robin((1..= 4).collect(), false);
+///let rounds1 = round_robin((1..= 4).collect());
 ///print!("{:?}", rounds1);
 ///```
-///Output: [[(1, 4), (2, 3)], [(1, 3), (4, 2)], [(1, 2), (3, 4)]]
+///Output: [[(1,4), (2,3)], [(1,3), (4,2)], [(1,2), (3,4)]] | 
 ///
 ///#### Examples with strings:
 ///```
-///let rounds2 = round_robin(vec!["a", "b", "c", "d"], false);
+///let rounds2 = round_robin(vec!["a", "b", "c", "d"]);
 ///print!("{:?}", rounds2);
 ///```
 ///Output: [[("a", "d"), ("b", "c")], [("a", "c"), ("d", "b")], [("a", "b"), ("c", "d")]]
